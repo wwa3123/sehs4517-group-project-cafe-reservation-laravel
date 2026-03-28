3a8b7ecaec648894323dabafb68261c087c7763a
Timothy <25131108s@common.cpce-polyu.edu.hk>
2026-03-28 18:59:58 +0800

    feat: add registration page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistrationController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +10,14 @@ Route::get('/', function () {
 
 Route::get('/tailwind', function () {
     return view('tailwindTest');
+});
+
+Route::get('/register', function() {
+    return view('register');
+});
+Route::post('/register', [RegistrationController::class, 'register']);
+Route::get('/check-email', [RegistrationController::class, 'checkEmail']);
+Route::post('/forget-old', function() {
+    session()->forget('_old_input');
+    return response()->json(['status' => 'success']);
 });
