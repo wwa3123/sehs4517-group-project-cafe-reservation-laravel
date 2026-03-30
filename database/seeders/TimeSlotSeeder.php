c281680741936042bb9a4f242f2a3e114cbaf0b0
Yiu Pak Hei Patrick <25054701S@common.cpce-polyu.edu.hk>
2026-03-30 17:25:19 +0800

    feat: Update TimeSlotSeeder to adjust time slots for reservations

--- database/seeders/TimeSlotSeeder.php
+++ database/seeders/TimeSlotSeeder.php
@@ -17,23 +17,48 @@ class TimeSlotSeeder extends Seeder
     public function run(): void
     {
         TimeSlot::create([
-            'start_time' => Carbon::now()->setTime(10, 0),
-            'end_time' => Carbon::now()->setTime(12, 30),
+            'start_time' => Carbon::now()->setTime(14, 0),
+            'end_time' => Carbon::now()->setTime(15, 0),
         ]);
 
         TimeSlot::create([
-            'start_time' => Carbon::now()->setTime(13, 0),
-            'end_time' => Carbon::now()->setTime(15, 30),
+            'start_time' => Carbon::now()->setTime(15, 0),
+            'end_time' => Carbon::now()->setTime(16, 0),
         ]);
 
         TimeSlot::create([
             'start_time' => Carbon::now()->setTime(16, 0),
-            'end_time' => Carbon::now()->setTime(18, 30),
+            'end_time' => Carbon::now()->setTime(17, 0),
+        ]);
+
+        TimeSlot::create([
+            'start_time' => Carbon::now()->setTime(17, 0),
+            'end_time' => Carbon::now()->setTime(18, 0),
+        ]);
+
+        TimeSlot::create([
+            'start_time' => Carbon::now()->setTime(18, 0),
+            'end_time' => Carbon::now()->setTime(19, 0),
         ]);
 
         TimeSlot::create([
             'start_time' => Carbon::now()->setTime(19, 0),
-            'end_time' => Carbon::now()->setTime(21, 30),
+            'end_time' => Carbon::now()->setTime(20, 0),
+        ]);
+
+        TimeSlot::create([
+            'start_time' => Carbon::now()->setTime(20, 0),
+            'end_time' => Carbon::now()->setTime(21, 0),
+        ]);
+
+        TimeSlot::create([
+            'start_time' => Carbon::now()->setTime(21, 0),
+            'end_time' => Carbon::now()->setTime(22, 0),
+        ]);
+
+        TimeSlot::create([
+            'start_time' => Carbon::now()->setTime(22, 0),
+            'end_time' => Carbon::now()->setTime(23, 0),
         ]);
     }
 }
