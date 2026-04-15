1e38de54cb875182a04f043acabbfda5822e9d56
Timothy <25131108s@common.cpce-polyu.edu.hk>
2026-04-15 21:10:14 +0800

    chore: revert files for cleaner diff

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,9 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
 
-class User extends Authenticatable {
+class User extends Authenticatable
+{
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
@@ -19,14 +19,9 @@ class User extends Authenticatable {
      * @var list<string>
      */
     protected $fillable = [
-        'first_name',
-        'last_name',
-        'address',
-        'phone',
+        'name',
         'email',
-        'password_hash',
-        'loyalty_points',
-        'subscribe_events'
+        'password',
     ];
 
     /**
@@ -44,22 +39,11 @@ class User extends Authenticatable {
      *
      * @return array<string, string>
      */
-    protected function casts(): array {
+    protected function casts(): array
+    {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public static function registerUser($data) {
-        return self::create([
-            'first_name' => $data['first_name'],
-            'last_name' => $data['last_name'],
-            'address' => $data['address'],
-            'phone' => $data['phone'],
-            'email' => $data['email'],
-            'password_hash' => Hash::make($data['password']),
-            'subscribe_events' => request()->boolean('subscribe'),
-        ]);
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,15 +13,12 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('address');
-            $table->string('phone');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->string('password_hash');
-            $table->integer('loyalty_points')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
-            $table->boolean('subscribe_events')->default(false);
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
