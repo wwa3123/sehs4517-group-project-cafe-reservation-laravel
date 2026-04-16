29eead98a92433c3aca4d38152b025d8bfafcaf4
W25131108S <25131108s@common.cpce-polyu.edu.hk>
2026-04-16 18:55:16 +0800

    Update app/Http/Controllers/RegistrationController.php
    Co-authored-by: phyiu-patrick <25054701S@common.cpce-polyu.edu.hk>

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Member;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\RegistrationRequest;
