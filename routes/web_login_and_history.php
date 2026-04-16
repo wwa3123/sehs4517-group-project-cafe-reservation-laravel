ece88c49a144c38e50cd901474c65573e46fe7f4
phyiu-patrick <25054701S@common.cpce-polyu.edu.hk>
2026-04-16 18:32:23 +0800

    Update routes/web_login_and_history.php
    Co-authored-by: W25131108S <25131108s@common.cpce-polyu.edu.hk>

--- routes/web_login_and_history.php
+++ routes/web_login_and_history.php
@@ -7,9 +7,7 @@ use App\Http\Controllers\HistoryController;
 // login routes
 Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/login/verify', [LoginController::class, 'verify'])->name('login.verify');
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-
-// reservation history route
-Route::get('/reservation/history', [HistoryController::class, 'index'])
-    ->name('reservation.history')
-    ->middleware('check.member'); 
+Route::middleware('auth')->group(function() {
+    Route::get('/reservation/history', [HistoryController::class, 'index'])->name('reservation.history');
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+});
