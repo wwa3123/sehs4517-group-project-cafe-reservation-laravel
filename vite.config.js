dd9f3f72e099b0b7c2f58a792a70db92f5f176f5
Yiu Pak Hei Patrick <25054701S@common.cpce-polyu.edu.hk>
2026-04-17 21:46:55 +0800

    Add scoped styles for login page and update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/validation.js'],
+            input: ['resources/css/app.css', 'resources/css/login.css', 'resources/js/app.js', 'resources/js/validation.js'],
             refresh: true,
         }),
         tailwindcss(),
